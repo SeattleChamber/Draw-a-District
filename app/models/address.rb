4498a841fe2ec8e115f7702edb0b8b495c857fd3
Anne Kimsey <anne.kimsey@gmail.com>
2014-04-02 12:07:19 -0700

    CSV upload works

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,12 @@ class Address
   field :lat, type: String
   field :long, type: String
   field :district, type: String
+  field :user_id, type: Integer
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      Address.create! row.to_hash
+    end
+  end
+
 end
