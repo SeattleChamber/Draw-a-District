e1f7a107b17b32bf741d211e26177bbdc844cd51
Anne Kimsey <anne.kimsey@gmail.com>
2014-04-03 11:02:32 -0700

    displays a map with overlays and drops pins from the addresses...on a different map

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,11 @@
 class WelcomeController < ApplicationController
+
+  def home
+    @addresses = Address.all
+    @hash = Gmaps4rails.build_markers(@addresses) do |address, marker|
+      marker.lat address.lat
+      marker.lng address.long
+    end
+  end
+
 end
