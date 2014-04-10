6e3bb4ed0540addb36fd1a1905fda07402cae5e9
Anne Kimsey <anne.kimsey@gmail.com>
2014-04-10 10:52:45 -0700

    adding logic to work with pinp

--- app/models/address.rb
+++ app/models/address.rb
@@ -9,11 +9,57 @@ class Address
   include Geocoder::Model::Mongoid
   geocoded_by :text               # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
+  require 'pinp'
+
+  def initialize
+    # these should have the coordinate sets in them 
+    @district1a = []
+    @district1b = []
+    @district1c = []
+    @district2 = []
+    @district3a = []
+    @district3b = []
+    @district3c = []
+    @district3d = []
+    @district4a = []
+    @district4b = []
+    @district5 = []
+    @district6 = []
+    @district7 = []
+  end
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       Address.create! row.to_hash
     end
+  end
+
+  def district1
+  end
+
+  def district2
+  end
+
+  def district3
+  end
+
+  def district4
+  end
+
+  def district5
+  end
+
+  def district6
+  end
+
+  def district7
+  end
+
+  def make_polygon(points)
+    points.each do |coordinate_set|
+      perimeter << Pinp::Point.new(coordinate_set[0], coordinate_set[1])
+    end
+    @pgon = Pinp::Polygon.new perimeter
   end
 
 end
