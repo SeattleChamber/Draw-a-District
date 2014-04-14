c7080d754bd47e5c1bb48ea2d3314ee14ca095e0
Anne Kimsey <anne.kimsey@gmail.com>
2014-04-14 11:47:59 -0700

    removed redundant logic from welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,6 @@
 class WelcomeController < ApplicationController
 
   def home
-    @addresses = Address.all
-    @hash = Gmaps4rails.build_markers(@addresses) do |address, marker|
-      marker.lat address.to_coordinates[0]
-      marker.lng address.to_coordinates[1]
-    end
-    gon.push({
-      :hash => @hash 
-    })
   end
 
 end
