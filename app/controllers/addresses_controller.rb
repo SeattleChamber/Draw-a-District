c51b5a11a3ef23ea4a67425267ed9375451b0b04
Anne Kimsey <anne.kimsey@gmail.com>
2014-04-14 11:45:27 -0700

    removed useless show logic

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -5,21 +5,6 @@ class AddressesController < ApplicationController
     redirect_to addresses_path, notice: "CSV successfully imported."
   end
 
-  def show
-    @addresses = Address.all
-    @hash = Gmaps4rails.build_markers(@addresses) do |address, marker|
-      marker.lat address.to_coordinates[0]
-      marker.lng address.to_coordinates[1]
-    end
-    gon.push({
-      :hash => @hash
-    })
-    respond_to do |format|
-      format.html
-      format.csv { render text: @addresses.to_csv }
-    end
-  end
-
   def index
     @addresses = Address.all
     @hash = Gmaps4rails.build_markers(@addresses) do |address, marker|
