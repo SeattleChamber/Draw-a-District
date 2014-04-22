4d9bee0491b39f383fe49bd366267748c50c8458
Anne Kimsey <anne.kimsey@gmail.com>
2014-04-21 20:35:37 -0700

    working on ux based around documents instead of addresses

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -6,6 +6,19 @@ class DocumentsController < ApplicationController
 
   def show
     @document = Document.find(params[:id])
+    @addresses = @document.addresses
+    @hash = Gmaps4rails.build_markers(@addresses) do |address, marker|
+      marker.lat address.to_coordinates[0]
+      marker.lng address.to_coordinates[1]
+    end
+    gon.push({
+      :hash => @hash
+    })
+    respond_to do |format|
+      format.html
+      format.csv { render text: @addresses.to_csv }
+      format.xls { send_data @addresses.to_csv(col_sep: "\t") }
+    end
   end
 
 end
