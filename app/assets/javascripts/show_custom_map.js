49d4c1fc804b446fe7e3d543a9d054f4db671f2b
Anne Kimsey <anne.kimsey@gmail.com>
2014-04-22 17:27:43 -0700

    formatted map zoom

--- app/assets/javascripts/show_custom_map.js
+++ app/assets/javascripts/show_custom_map.js
@@ -1,12 +1,14 @@
 window.onload = function() {
   handler = Gmaps.build('Google');
   handler.buildMap({ provider: {}, internal: {id: 'custom_map'}}, function(){
-    var poly = handler.addPolygons(
+    var polygon = handler.addPolygons(
     [
       gon.formatted_bounds
     ],
     { strokeColor: '#FF0000'}
   );
-    handler.getMap().setZoom(11);
+    handler.bounds.extendWith(polygon);
+    handler.fitMapToBounds();
+    handler.getMap().setZoom(8);
   });
 }
