1fee9f966b0eac76a9eb11c727d0503525ac2527
Anne Kimsey <anne.kimsey@gmail.com>
2014-10-30 12:21:56 -0700

    some styling to indicate that the user is signed in as an admin

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
 
   def home
-    if current_user
+    if current_user && current_user.admin == true
+      flash[:notice] = "Welcome Admin User: #{current_user.email}"
+      redirect_to documents_path
+    elsif current_user
       redirect_to documents_path
     else
       render layout: "headless"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User
   include Mongoid::Document
+  field :admin, type: Boolean
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
